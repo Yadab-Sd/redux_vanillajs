6d532846ea754e42624e3498acbb2fc32ef8ac38
Yadab-Sd <yadab.sd2013@gmail.com>
2020-09-12 22:18:34 +0600

    first commit completed

---
+++ src/main.js
@@ -0,0 +1,69 @@
+import store from "./store/store";
+import { addNote } from "./actions/actions";
+
+// We use store.getState() to get our app state from the store
+
+console.log("Before:", store.getState());
+store.dispatch(addNote("One", "One content"));
+store.dispatch(addNote("Two", "Two content"));
+store.dispatch(addNote("Three", "Three content"));
+console.log("After:", store.getState());
+
+// ------ HTML references ------
+let notesUList = document.getElementById("notes");
+let addNoteForm = document.getElementById("add-note");
+let addNoteTitle = addNoteForm["title"];
+let addNoteContent = addNoteForm["content"];
+
+// ------ Redux ------
+function deleteNote(index) {
+  store.dispatch(removeNote(index));
+}
+
+function insertNote(title, content) {
+  store.dispatch(addNote(title, content));
+}
+
+function renderNotes() {
+  let notes = store.getState().notes;
+
+  notesUList.innerHTML = "";
+  notes.map((note, index) => {
+    let noteItem = `
+        <li>
+          <b>${note.title}</b>
+          <button data-id="${index}">x</button>
+          <br />
+          <span>${note.content}</span>
+        </li>
+      `;
+    notesUList.innerHTML += noteItem;
+  });
+
+  setDeleteNoteButtonsEventListeners();
+}
+
+// ------ Event Listeners ------
+addNoteForm.addEventListener("submit", (e) => {
+  let title = addNoteTitle.value;
+  let content = addNoteContent.value;
+  insertNote(title, content);
+});
+
+function setDeleteNoteButtonsEventListeners() {
+  let buttons = document.querySelectorAll("ul#notes li button");
+
+  for (let button of buttons) {
+    button.addEventListener("click", () => {
+      deleteNote(button.dataset.id);
+    });
+  }
+}
+
+// ------ Render the initial Notes ------
+renderNotes();
+
+// ----- Render after
+store.subscribe(() => {
+  renderNotes();
+});
